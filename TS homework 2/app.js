f1e5fe9c23e7c7c9e9e8bd4e4ccbaf98ee3058d3
Iryna Khylchuk <khylchuk@icloud.com>
2024-03-01 15:43:04 +0200

    TS Homework 2
    TS (2 tuples, readonly, enum, any...)

---
+++ TS homework 2/app.js
@@ -0,0 +1,31 @@
+"use strict";
+let user = ['John Doe', 27];
+console.log(`Name: ${user[0]},\nAge: ${user[1]} y.o.`);
+function getObjsByProp(arr, prop, val) {
+    return arr.filter(obj => obj[prop] === val);
+}
+const arrTask2 = [
+    { fruit: 'apple', color: 'red' },
+    { fruit: 'apple', color: 'green' },
+    { fruit: 'orange', color: 'orange' },
+    { fruit: 'kiwi', color: 'green' }
+];
+console.log(getObjsByProp(arrTask2, 'fruit', 'apple'));
+//! 3
+function getObjByProp(arr, prop, val) {
+    return arr.find(obj => obj[prop] === val ? obj : undefined);
+}
+const arrTask3 = [
+    { fruit: 'apple', color: 'red' },
+    { fruit: 'orange', color: 'orange' },
+    { fruit: 'kiwi', color: 'green' }
+];
+console.log(getObjByProp(arrTask3, 'fruit', 'apple'));
+//! 4
+function getArithmeticMean(arr) {
+    let sum = 0;
+    arr.forEach(num => sum += num);
+    let arithmeticMean = sum / arr.length;
+    return arithmeticMean;
+}
+console.log(getArithmeticMean([1, 2, 3, 4]));
---
+++ TS homework 2/app.ts
@@ -0,0 +1,50 @@
+//! 1
+type UserTuple = [string, number]
+let user: UserTuple = ['John Doe', 27]
+
+console.log(`Name: ${user[0]},\nAge: ${user[1]} y.o.`)
+
+
+//! 2
+interface Object {
+    [key: string]: any
+}
+
+function getObjsByProp(arr: Object[], prop: string, val: any): Object[] {
+    return arr.filter(obj => obj[prop] === val)
+}
+
+const arrTask2 = [
+    {fruit: 'apple', color: 'red'},
+    {fruit: 'apple', color: 'green'},
+    {fruit: 'orange', color: 'orange'},
+    {fruit: 'kiwi', color: 'green'}
+]
+
+console.log(getObjsByProp(arrTask2, 'fruit', 'apple'))
+
+
+//! 3
+function getObjByProp(arr: Object[], prop: string, val: any): Object | undefined {
+    return arr.find(obj => obj[prop] === val? obj : undefined)
+}
+
+const arrTask3 = [
+    {fruit: 'apple', color: 'red'},
+    {fruit: 'orange', color: 'orange'},
+    {fruit: 'kiwi', color: 'green'}
+]
+
+console.log(getObjByProp(arrTask3, 'fruit', 'apple'))
+
+
+//! 4
+function getArithmeticMean(arr: number[]): number {
+    let sum: number = 0
+    arr.forEach(num => sum += num)
+    let arithmeticMean = sum / arr.length
+
+    return arithmeticMean
+}
+
+console.log(getArithmeticMean([1, 2, 3, 4]))
